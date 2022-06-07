af5f07c0001d874c9f00436130e1f8545e81f6c9
aswin <aswinpdas83@gmail.com>
2022-06-07 15:35:30 +0530

    small change

--- demo_avodha_app/models.py
+++ demo_avodha_app/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class shop(models.Model):
     name=models.CharField(max_length=100)
     desc=models.TextField()
-    price=models.IntegerField
+    price=models.IntegerField()
