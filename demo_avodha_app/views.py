1edcd810fdf2575ce817bb2a04bdd7de50471e90
aswin <aswinpdas83@gmail.com>
2022-06-07 16:49:26 +0530

    feaching the product details and render the output in #details.html

--- demo_avodha_app/views.py
+++ demo_avodha_app/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from . models import shop
 # Create your views here.
 def demo(req):
-    return  render(req,'home.html')
+    obj_product=shop.objects.all()
+
+    return  render(req,'home.html',{'products':obj_product})
+def details(req,product_id):
+    product1=shop.objects.get(id=product_id)
+    return render(req,'details.html',{'product':product1})
