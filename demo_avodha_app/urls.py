16be9d614297b3606a05e3c46dd0e6b1935e5b8d
aswin <aswinpdas83@gmail.com>
2022-06-07 17:07:54 +0530

    change in demo_avodha_app/urls

---
+++ demo_avodha_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from  demo_avodha_app import views
+
+urlpatterns = [
+    path('',views.demo,name='demo'),
+    path('shop/<int:product_id>',views.details,name='details')
--- demo_avodha_project/urls.py
+++ demo_avodha_project/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from  demo_avodha_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.demo,name='demo'),
-    path('shop/<int:product_id>',views.details,name='details')
+    path('',include('demo_avodha_app/urls'))
 ]
