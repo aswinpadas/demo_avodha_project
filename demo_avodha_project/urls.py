0704826707aa64fb62579be8bae5200d5c7f5fdc
aswin <aswinpdas83@gmail.com>
2022-06-07 16:48:12 +0530

    products details featching with product id

--- demo_avodha_project/urls.py
+++ demo_avodha_project/urls.py
@@ -19,5 +19,6 @@ from  demo_avodha_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.demo,name='demo')
+    path('',views.demo,name='demo'),
+    path('shop/<int:product_id>',views.details,name='details')
 ]
